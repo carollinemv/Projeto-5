9f1576091bf9c8b64d9837885c182b07d80d1722
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-06-23 17:11:50 -0300

    Add files via upload

--- porta.js
+++ porta.js
@@ -2,8 +2,31 @@ const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById ('turnOff');
 const door = document.getElementById ('door');
 
-function doorOn () {
-    door.src = "portaberta.png"
+function isdoorBroken () {
+    return door.src.indexOf ('bu') > -1
 }
 
-turnOn.addEventListener ('click', doorOn)
+function doorOn () {
+    if (!isdoorBroken () ) {
+        door.src = "portaberta.png";
+    }
+    
+}
+
+function doorOff () {
+    if (!isdoorBroken () ) {
+        door.src = "portafechada.png";
+    }
+   
+}
+
+function doorBroken() {
+    door.src = "bu.png";
+}
+
+turnOn.addEventListener ('click', doorOn);
+turnOff.addEventListener ('click' , doorOff);
+door.addEventListener ( 'mouseover', doorOn);
+door.addEventListener ('mouseleave', doorOff);
+door.addEventListener ('dblclick', doorBroken);
+
